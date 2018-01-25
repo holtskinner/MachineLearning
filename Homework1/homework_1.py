0e4060797cff236c220d852d2b478eee52b48b80
Holt Skinner <HoltWasHere@gmail.com>
2018-01-24 20:15:14 -0600

    Added mean function

--- Homework1/homework_1.py
+++ Homework1/homework_1.py
@@ -1,7 +1,28 @@
 import scipy.io as sp
+import numpy as np
 
-data = sp.loadmat("./data_class4.mat")["Data"][0]
 
-print(data[0])
-for d in data[0]:
-    print(d)
+def mean(matrix):
+
+    total = 0
+    num_values = 0
+
+    for row in matrix:
+        for item in row:
+            num_values += 1
+            total += item
+
+    return 0 if num_values == 0 else total / (num_values)
+
+
+def main():
+    # DATA is a list of Matrices (3D Matrix!)
+    data = sp.loadmat("./data_class4.mat")["Data"][0]
+
+    print(np.mean(data[0]))
+
+    m = mean(data[0])
+    print(m)
+
+
+main()
