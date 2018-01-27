d5c93b3bef306fb46d17d0c6575622ec8acbd845
Holt Skinner <HoltWasHere@gmail.com>
2018-01-27 10:45:16 -0600

    Added -1 to covariance matrix division plus formatting

--- Homework1/homework_1.py
+++ Homework1/homework_1.py
@@ -41,43 +41,48 @@ def covariance(m):
                 cov_matrix[i][j] += (m[i][k] - mean_vector[i]) * (
                     m[j][k] - mean_vector[j])
 
-            cov_matrix[i][j] /= num_features
+            cov_matrix[i][j] /= num_features - 1
 
     return (cov_matrix, mean_vector)
 
+def eigenvalues_eigenvectors(m):
+
+    return 0
 
 def main():
 
     # DATA is a list of Matrices (3D Matrix!)
     data = sp.loadmat("./data_class4.mat")["Data"][0]
 
-    print("Mean & Covariance")
-
     # matrix Because class is a reserved word
     for index, matrix in enumerate(data):
+
         calculated_cov, mean_vector = covariance(matrix)
         numpy_cov = np.cov(matrix)
         numpy_mean_x = np.mean(matrix[0])
         numpy_mean_y = np.mean(matrix[1])
+
         print("\n---------------")
         print(f"Class {index}:")
         print("---------------\n")
+
         print("Covaraiance")
         print("By Hand:")
         print(calculated_cov)
         print("Built-In:")
         print(numpy_cov)
+
         print("\nMean")
         print("X")
         print(f"By Hand: {mean_vector[0]}")
         print(f"Built-In {numpy_mean_x}")
         print("Y")
         print(f"By Hand: {mean_vector[1]}")
-        print(f"Built-In {numpy_mean_y}")
+        print(f"Built-In {numpy_mean_y}\n")
 
-    # for matrix in data:
-    #     eigen = np.linalg.eigvals(matrix)
-    #     print(eigen)
-
+        print("Eigenvalues & Eigenvectors")
+        eigenvalues, eigenvectors = np.linalg.eig(calculated_cov)
+        print(eigenvalues)
+        print(eigenvectors)
 
 main()
