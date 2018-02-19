f1b090124d9875ae6363d3b6170b96e7ad9c34e3
Holt Skinner <HoltWasHere@gmail.com>
2018-02-18 19:23:25 -0600

    Added some of the graphing

--- Homework2/homework2.py
+++ Homework2/homework2.py
@@ -1,7 +1,9 @@
 import numpy as np
+from scipy.stats import multivariate_normal as mn
 import matplotlib.pyplot as plt
 from scipy.io.matlab import loadmat
 from scipy.spatial.distance import mahalanobis as mh
+from mpl_toolkits.mplot3d import Axes3D
 
 
 # x1 and x2 are points in 3-dimensions (1D Array with 3 items), I think?
@@ -48,7 +50,7 @@ def whiten(y, cov_matrix):
     # Whitening Matrix
     W = np.dot(np.dot(eigenvectors, D), eigenvectors.T)
 
-    return
+    return W
 
 
 def part1():
@@ -85,23 +87,40 @@ def part1():
 def part2():
 
     # Part a, b, c
-    mean_vectors = np.array([[8, 2], [2, 8]])
-    cov_matrix = np.array([[4.1, 0], [0, 2.8]])
+    # mean_vectors = np.array([[8, 2], [2, 8]])
+    # cov_matrix = np.array([[4.1, 0], [0, 2.8]])
 
-    y = np.random.standard_normal(1000)
+    # y = np.random.standard_normal(1000)
 
-    whiten(y, cov_matrix)
+    u1 = [8, 2]
+    u2 = [2, 8]
+    cov1 = [[4.1, 0], [0, 2.8]]
+    cov2 = cov1
+    prior1 = .8
 
-    # x = np.random.multivariate_normal(mean_vectors[1], cov_matrix, 1000).T
+    data_points = np.random.multivariate_normal(u1, cov1, 50).T
+    # print(xy)
+
+    X, Y = np.meshgrid(data_points[0], data_points[1])
+    xy = np.column_stack([X.flat, Y.flat])
+
+    z = mn.pdf(xy, mean=u1, cov=cov1)
+    Z = z.reshape(X.shape)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, linewidth=0, antialiased=True)
+    plt.show()
+
     # print(x)
 
     # Part d
-    cov_matrix[0, 1] = 0.4
-    cov_matrix[1, 0] = 0.4
+    # cov_matrix[0, 1] = 0.4
+    # cov_matrix[1, 0] = 0.4
 
-    # Part e
-    cov_matrix1 = np.array([[2.1, 1.5], [1.5, 3.8]])
-    cov_matrix2 = cov_matrix
+    # # Part e
+    # cov_matrix1 = np.array([[2.1, 1.5], [1.5, 3.8]])
+    # cov_matrix2 = cov_matrix
 
     return
 
