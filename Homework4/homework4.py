e30ec1ae03ac6295b2b83a0e61523dede064ca7d
Holt Skinner <holt@umc-143026.dhcp.missouri.edu>
2018-03-21 18:57:33 -0500

    Added Part B, C, D

--- Homework4/homework4.py
+++ Homework4/homework4.py
@@ -2,16 +2,31 @@ import numpy as np
 from data import data
 
 
+def print_mu_sigma(mu, sigma):
+    print("μ")
+    print(mu)
+    print("σ")
+    print(sigma)
+    print()
+
+
 def max_likelihood(x, n):
 
     x = x.T
-    mu = np.sum(x, axis=0) / n
+    mu = 0
 
-    d = x - mu
+    for k in range(n):
+        mu += x[k]
 
-    dt = np.multiply(d, d)
+    mu /= n
 
-    sigma = np.sum(dt, axis=0) / (n - 1)
+    sigma = 0
+
+    for k in range(n):
+        d = x[k] - mu
+        sigma += d * d.T
+
+    sigma /= (n)
 
     return mu, sigma
 
@@ -24,9 +39,30 @@ data_by_features = np.transpose(data, (2, 1, 0))
 
 data_by_class = np.transpose(data, (1, 2, 0))
 
+class1 = data_by_class[0]
+print("Part A\n")
+
 for i in range(num_classes):
     mu, sigma = max_likelihood(data_by_class[0, i], num_samples)
-    print(f"Class {i}")
-    print("μ       σ")
-    print(mu, sigma)
-    print()
+    print(f"Feature {i}")
+    print_mu_sigma(mu, sigma)
+
+print("Part B\n")
+combinations = np.array([
+    [class1[0], class1[1]],
+    [class1[0], class1[2]],
+    [class1[1], class1[2]]
+])
+
+for b in combinations:
+    mu, sigma = max_likelihood(b, num_samples)
+    print_mu_sigma(mu, sigma)
+
+print("Part C\n")
+mu, sigma = max_likelihood(class1, num_samples)
+print_mu_sigma(mu, sigma)
+
+
+print("Part D\n")
+mu, sigma = max_likelihood(data_by_class[1], num_samples)
+print_mu_sigma(mu, sigma)
