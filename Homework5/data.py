614246e0bd54277f5bd298a61acee068c26e3f21
Holt Skinner <holt@Holts-MacBook-Pro.local>
2018-04-17 11:13:30 -0500

    Separated out data manipulation

---
+++ Homework5/data.py
@@ -0,0 +1,32 @@
+import numpy as np
+from scipy.io.matlab import loadmat
+
+
+def load_data():
+
+    train, test = loadmat("./test_train_data_class3.mat")["Data"][0][0]
+
+    train = np.array(train[0])
+    test = np.array(test[0])
+
+    for i in range(train.shape[0]):
+        train[i] = np.transpose(train[i])
+
+    for i in range(test.shape[0]):
+        test[i] = np.transpose(test[i])
+    return train, test
+
+
+def flatten_data(data, c):
+    actual = np.array([], dtype=int)
+    flat = np.zeros(2)
+
+    # Flatten Test Array
+    for i in range(c):
+        for j in range(data[i].shape[0]):
+            actual = np.append(actual, i)
+            flat = np.vstack([flat, data[i][j]])
+
+    flat = flat[1:101]
+
+    return flat, actual
--- Homework5/homework5.py
+++ Homework5/homework5.py
@@ -1,38 +1,8 @@
 import numpy as np
-from scipy.io.matlab import loadmat
+from data import load_data, flatten_data
 from mahalanobis import discriminant
 from confusion_matrix import confusion_matrix
 from coordinates import cart2pol, pol2cart
-
-
-def load_data():
-
-    train, test = loadmat("./test_train_data_class3.mat")["Data"][0][0]
-
-    train = np.array(train[0])
-    test = np.array(test[0])
-
-    for i in range(train.shape[0]):
-        train[i] = np.transpose(train[i])
-
-    for i in range(test.shape[0]):
-        test[i] = np.transpose(test[i])
-    return train, test
-
-
-def flatten_data(data, c):
-    actual = np.array([], dtype=int)
-    flat = np.zeros(2)
-
-    # Flatten Test Array
-    for i in range(c):
-        for j in range(data[i].shape[0]):
-            actual = np.append(actual, i)
-            flat = np.vstack([flat, data[i][j]])
-
-    flat = flat[1:101]
-
-    return flat, actual
 
 
 def main():
@@ -78,7 +48,7 @@ def main():
 
     flat_train, _ = flatten_data(train, c)
 
-    polar = cart2pol()
+    # polar = cart2pol()
 
 
 if __name__ == "__main__":
