544b7f39cae6d76f8c160bba3a8944a4bf49d6da
Holt Skinner <holt@Holts-MacBook-Pro.local>
2018-04-17 10:12:57 -0500

    Got the Data Manipulation Working!

--- Homework5/homework5.py
+++ Homework5/homework5.py
@@ -1,13 +1,12 @@
 import numpy as np
 from scipy.io.matlab import loadmat
-from max_likelihood import mle
 from mahalanobis import discriminant
+from sklearn.metrics import confusion_matrix
 
 
-def load_data(train, test):
+def load_data():
 
-    # train, test =
-    # print(train, test)
+    train, test = loadmat("./test_train_data_class3.mat")["Data"][0][0]
 
     train = np.array(train[0])
     test = np.array(test[0])
@@ -17,28 +16,50 @@ def load_data(train, test):
 
     for i in range(test.shape[0]):
         test[i] = np.transpose(test[i])
-
     return train, test
 
 
 def main():
 
-    train, test = loadmat("./test_train_class3.mat")["Data"][0][0]
+    train, test = load_data()
 
-    means = np.empty(train.shape[0])
-    covs = np.empty(train.shape[0])
-
-    # Part A
-    for i in range(train.shape[0]):
-        means[i], covs[i] = mle(train[i])
-
-    # Part B
     prior = 1 / 3
-    d = train.shape[1]
-    print(d)
 
-    for i in range(train.shape[0]):
-        m = discriminant(train[i], means[i], covs[i], d, prior)
+    # Num Classes
+    c = train.shape[0]
+    # Num Dimensions
+    d = train[0].shape[1]
+
+    means = np.empty((c, d))
+    covs = np.empty(((c, d, d)))
+
+    for i in range(c):
+        means[i] = np.mean(train[i], axis=0)
+        covs[i] = np.cov(train[i], rowvar=0)
+
+    expected = np.array([], dtype=int)
+    actual = np.array([], dtype=int)
+
+    flat_test = np.zeros(2)
+
+    # Flatten Test Array
+    for i in range(c):
+        for j in range(test[i].shape[0]):
+            expected = np.append(expected, i)
+            flat_test = np.vstack([flat_test, test[i][j]])
+
+    flat_test = flat_test[1:101]
+    disc_values = np.zeros((100, 3))
+
+    for i, point in enumerate(flat_test):
+        for j in range(c):
+            m = discriminant(point, means[j], covs[j], d, prior)
+            disc_values[i, j] = m
+
+    actual = np.argmax(disc_values, axis=1)
+
+    print(expected)
+    print(actual)
 
 
 if __name__ == "__main__":
--- Homework5/mahalanobis.py
+++ Homework5/mahalanobis.py
@@ -1,20 +1,19 @@
 import numpy as np
 
 
-def mahalanobis(x1, x2, covariance):
+def mah(x1, x2, cov):
 
     # Formula distance = √(x1 - x2)T ∑-1 (x1-x2)
     # numpy arrays have built in vector addition & subtraction!
-    difference_vector = x1 - x2
+    diff = x1 - x2
 
     # Vector-Matrix Multiplication
     # first pair (numpy only allows two at a time)
-    distance = np.dot(difference_vector, np.linalg.inv(covariance))
+    dist = np.dot(diff, np.linalg.inv(cov))
 
-    # Not taking the square root because we need the "squared distance" for the discriminant"
-    distance = np.dot(distance, difference_vector)
+    dist = np.dot(dist, diff)
 
-    return distance
+    return dist
 
 
 def discriminant(x, mean, covariance, dimension, prior):
@@ -22,7 +21,7 @@ def discriminant(x, mean, covariance, dimension, prior):
     # g(x) = (-1/2) square(mahalanobis(x, mu)) - (d / 2)ln(2pi)
     #  - (1 / 2)ln(det(cov)) + ln(prior)
 
-    a = (1 / 2) * mahalanobis(x, mean, covariance)
+    a = (1 / 2) * mah(x, mean, covariance)
 
     # np.log is natural log
     b = (dimension / 2) * np.log(2 * np.pi)
